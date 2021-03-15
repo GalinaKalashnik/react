2ca25c5bc26c9220fd7fd1e4a756aec8af343185
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2021-03-15 08:04:30 +0200

    start: routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import styles from './Navbar.module.css';
+import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return <nav className={styles.nav}>
-        <div className={styles.item}>
-            <a>Profile</a>
-        </div>
-        <div className={styles.item}>
-            <a>Messages</a>
-        </div>
-        <div className={styles.item}>
-        <a>News</a>
-        </div>
-        <div className={styles.item}>
-            <a>Music</a>
-        </div>
-        <div className={styles.item}>
-            <a>Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news' activeClassName={s.active}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './MyPosts.module.css';
+import s from './MyPosts.module.css';
 import Post from './Post/Post'
 
 const MyPosts = () => {
@@ -10,7 +10,7 @@ const MyPosts = () => {
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
-            <div className={styles.posts}>
+            <div className={s.posts}>
                 <Post messages='hi how are you' likesCount='0' />
                 <Post messages='bla bla bla ' likesCount='23' />
             </div>
