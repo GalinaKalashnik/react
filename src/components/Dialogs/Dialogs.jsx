930b74d7ae7118209851e29ee2a21f65f59a139c
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2021-04-19 14:17:59 +0300

    dialogs main structure

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,31 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div className={s.dialogs} >
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+               <div className={s.dialog}>
+                   Anna
+               </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sveta
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    hi
+                </div>
+                <div className={s.message}>
+                    hihiuiui
+                </div>
+                <div className={s.message}>
+                    hisdfsdfsdfsd
+                </div>
+                {/*<Message message={messagesData[0].message}/>*/}
+                {/*<Message message={messagesData[1].message}/>*/}
+            </div>
         </div>
     )
 };
