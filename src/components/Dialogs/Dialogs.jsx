58a7f397bc634d8a65598a28ac464a99fd960dfe
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2021-04-21 12:11:10 +0300

    22 dialogs main structure

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import s from './Dialogs.module.css';
+import React from "react";
+import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                <div className={s.dialog}>
-                   Anna
+                   <NavLink to="/dialogs/1">Anna</NavLink>
                </div>
                 <div className={s.dialog}>
-                    Alex
+                    <NavLink to="/dialogs/2">Alex</NavLink>
                 </div>
                 <div className={s.dialog + ' ' + s.active}>
-                    Sveta
+                    <NavLink to="/dialogs/3">Sveta</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
