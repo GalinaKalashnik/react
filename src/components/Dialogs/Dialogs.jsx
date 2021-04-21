95baed0da927c1dae25dfb50b1826ee27cdbc3f9
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2021-04-21 17:57:42 +0300

    26 dialog stucture create sub components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message"
 
-const DialogItem =(props) => {
-    let path = "/dialogs/" + props.id
-    return (
-        <div className={s.dialog}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message =(props) => {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-}
 const Dialogs = (props) => {
 
     let dialogs = [
@@ -36,7 +23,7 @@ const Dialogs = (props) => {
     ]
 
     let dialogsElements =  dialogs
-        .map(d => <DialogItem name={d.name} id={d.id} /> )
+        .map(d => <DialogItem name={props.d.name} id={props.d.id} /> )
 
     let messadesElements = messages
         .map( m => <Message message={m.message} id={m.id} />)
