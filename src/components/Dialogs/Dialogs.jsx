0522a9a2f9e04b6f78e5ab439748b33085013497
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2021-04-28 11:00:32 +0300

    28 выносим данные в index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,28 +5,11 @@ import Message from "./Message/Message"
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrew'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Viktor'},
-        {id: 6, name: 'Valera'}
-    ]
+    let dialogsElements =  props.dialogs
+        .map(d => <DialogItem name={d.name} id={d.id} key={d.id}/> )
 
-    let messages = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your it-kamasutra?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'eeee'},
-        {id: 5, message: 'Yo'}
-    ]
-
-    let dialogsElements =  dialogs
-        .map(d => <DialogItem name={props.d.name} id={props.d.id} /> )
-
-    let messadesElements = messages
-        .map( m => <Message message={m.message} id={m.id} />)
+    let messadesElements = props.messages
+        .map( m => <Message message={m.message} id={m.id} key={m.id} />)
 
     return (
         <div className={s.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 11},
-        {id: 2, message: 'It\'s my first post', likesCount: 11},
-        {id: 3, message: 'Blabla', likesCount: 11},
-        {id: 4, message: 'Dadsssa', likesCount: 11}
-    ]
-
-    let postsElements = posts
+    let postsElements = props.posts
         .map(p => <Post messages={p.message} likesCount={p.likesCount} />)
 
     return (
